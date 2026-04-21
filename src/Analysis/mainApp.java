65095d7e92c698c0717593c1f8b23e8b66c79f87
devckddhks <dev.ckddhks@gmail.com>
2026-04-21 17:30:28 +0900

    fix : parse 부분만 남깁니다.

--- src/Analysis/mainApp.java
+++ src/Analysis/mainApp.java
@@ -1,19 +1,9 @@
 package Analysis;
 
-import java.io.IOException;
-
-import OutputExport.AnalysisService;
-import OutputExport.ReportService;
-
 public class mainApp {
 
 	public static void main(String[] args) {
 		System.out.println("LogAnalysis Start");
-		try {
-			new AnalysisService();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 
 }
