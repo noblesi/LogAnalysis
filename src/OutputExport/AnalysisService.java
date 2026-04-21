aee6ae6e734361bfe07099350671d5765e2a6f26
parkjeungwook <ddoddo1213@gmail.com>
2026-04-21 17:26:53 +0900

    Update AnalysisService.java

--- src/OutputExport/AnalysisService.java
+++ src/OutputExport/AnalysisService.java
@@ -1,6 +1,9 @@
 package OutputExport;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,16 +17,23 @@ public class AnalysisService {
 	AnalysisResult analysisResult = new AnalysisResult();
 	List<LogDTO> logList = null;
 
-	public AnalysisService() throws IOException {
+	public AnalysisService(String filePath) throws IOException {
 		logList = new LogParse().getLogList();
+		
+		File file = new File(filePath);
+		analysisResult.setSourceFileName(file.getName());
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date d = new Date(file.lastModified());
+		analysisResult.setLogCreatedDate(sdf.format(d));
 
 		countKey();
 		countBrowser();
 		countStatusCode();
 		countPeakHour();
-		countforBidden403();
+		countForBidden403();
 		countBooks500();
-		
+
 	}
 
 	// 1. 최다 사용 키를 구하고 DTO 에 저장
@@ -65,7 +75,7 @@ public class AnalysisService {
 		}
 
 		for (String key : countMap.keySet()) {
-			perMap.put(key, ((double) countMap.get(key) / logList.size()));
+			perMap.put(key, ((double) countMap.get(key) / logList.size())*100);
 		}
 
 		analysisResult.setBrowserCount(countMap);
@@ -94,84 +104,117 @@ public class AnalysisService {
 		analysisResult.setSuccess200Count(success200Count);
 		analysisResult.setFail404Count(fail404Count);
 	}
-	
-	//4. 요청이 가장 많은 시간 
+
+	/**
+	 * 요청이 가장 많은 시간
+	 */
 	public void countPeakHour() {
 		Map<Integer, Integer> hourMap = new HashMap<Integer, Integer>();
-		
-		for(LogDTO dto : logList ) {
-			if(dto.getTime() != null) {
+
+		for (LogDTO dto : logList) {
+			if (dto.getTime() != null) {
 				int hour = dto.getTime().getHour();
-				
-				hourMap.put(hour, hourMap.getOrDefault(hour,0)+1);
-			}//end if 
-		}//end for
-		
+
+				hourMap.put(hour, hourMap.getOrDefault(hour, 0) + 1);
+			} // end if
+		} // end for
+
 		int peakHour = -1;
 		int maxCnt = 0;
-		
-		for (Integer hour : hourMap.keySet()) {
-	        if (hourMap.get(hour) > maxCnt) {
-	            peakHour = hour;
-	            maxCnt = hourMap.get(hour);
-	        }//end if 
-	    }//end for
 
-	    // AnalysisResult에 저장
-	    analysisResult.setPeakHour(peakHour);
-	}//countPeakHour
-	//5. 비정상적인 요청이 발생한 횟수 비율 
-	public void countforBidden403() {
+		for (Integer hour : hourMap.keySet()) {
+			if (hourMap.get(hour) > maxCnt) {
+				peakHour = hour;
+				maxCnt = hourMap.get(hour);
+			} // end if
+		} // end for
+
+		// AnalysisResult에 저장
+		analysisResult.setPeakHour(peakHour);
+	}// countPeakHour
+
+	/**
+	 * 비정상적인 요청이 발생한 횟수 403
+	 */
+	public void countForBidden403() {
 		int forbiddenCnt = 0;
-		for(LogDTO dto : logList ) {
+		for (LogDTO dto : logList) {
 			if (dto.getResponseResult() == StatusCode.FORBIDDEN) {
 				forbiddenCnt++;
-			}//end if 
-		}//end for 
+			} // end if
+		} // end for
 		double forbiddenRate = 0.0;
-		if(!logList.isEmpty()) {
-			forbiddenRate = ((double)forbiddenCnt/logList.size())*100;
-		}//end if 
+		if (!logList.isEmpty()) {
+			forbiddenRate = ((double) forbiddenCnt / logList.size()) * 100;
+		} // end if
 		analysisResult.setForbidden403Count(forbiddenCnt);
 		analysisResult.setForbidden403Rate(forbiddenRate);
-	}//forbidden403Count
-	//6. Books에 대한 요청중 url중 에러가 발생한 횟수,비율 구하기
+	}// forbidden403Count
+
+	/**
+	 * Books에 대한 요청중 url중 에러가 발생한 횟수,비율 구하기
+	 */
 	public void countBooks500() {
 		int booksRequestCnt = 0;
 		int books500Cnt = 0;
-		
-		for(LogDTO dto : logList) {
-			if("books".equals(dto.getUrl())) {
+
+		for (LogDTO dto : logList) {
+			if ("books".equals(dto.getUrl())) {
 				booksRequestCnt++;
-				if(dto.getResponseResult() == StatusCode.SERVER_ERROR) {
+				if (dto.getResponseResult() == StatusCode.SERVER_ERROR) {
 					books500Cnt++;
-				}//end if
-			}//end if
-		}//end for 
-		
+				} // end if
+			} // end if
+		} // end for
+
 		double books500Rate = 0.0;
-		if(booksRequestCnt > 0) {
-			books500Rate =((double)books500Cnt/logList.size())*100;
-		}//end if
-		
+		if (booksRequestCnt > 0) {
+			books500Rate = ((double) books500Cnt / booksRequestCnt) * 100;
+		} // end if
+
 		analysisResult.setBooks500Count(books500Cnt);
 		analysisResult.setBooks500Rate(books500Rate);
-	}//books500Count
-	//7.입력되는 라인에 해당하는 정보출력
-	public RangeResult analysisRange(int StartLine, int endLine) {
-//		RangeResult range = new RangeResult();
-//		
-//		range.setStartLine(StartLine);
-//		range.setEndLine(endLine);
-//		
-//		Map<String, Integer> keyCountMap = new HashMap<String, Integer>();
-//		
-//		for(LogDTO dto : logList) {
-//			int line = dto.getLineNumber();
-//		}//end for
-		return null;
-	}//rangeResult
-	
+	}// books500Count
+
+	/**
+	 * 입력되는 라인에 해당하는 정보출력
+	 * 
+	 * @param StartLine 시작 줄
+	 * @param endLine   끝나는 줄
+	 * @return
+	 */
+	public RangeResult analysisRange(int startLine, int endLine) {
+		RangeResult range = new RangeResult();
+		range.setStartLine(startLine);
+		range.setEndLine(endLine);
+
+		Map<String, Integer> countMap = new HashMap<>();
+
+		for (LogDTO dto : logList) {
+			int line = dto.getLineNumber();
+			if (line >= startLine && line <= endLine) {
+				String key = dto.getKey();
+				if (key != null && !"".equals(key)) {
+					countMap.put(key, countMap.getOrDefault(key, 0) + 1);
+				}//end if 
+			}//end if
+		}//end for
+
+		String topKey = "";
+		int topCount = 0;
+
+		for (String key : countMap.keySet()) {
+			if (countMap.get(key) > topCount) {
+				topKey = key;
+				topCount = countMap.get(key);
+			}//end if 
+		}//end for
+
+		range.setTopKey(topKey);
+		range.setTopKeyCount(topCount);
+		return range;
+	}//analysisRange
+
 	public AnalysisResult getAnalysisResult() {
 		return analysisResult;
 	}
